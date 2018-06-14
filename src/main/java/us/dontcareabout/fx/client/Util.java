88605ae8653cf851270236e81b5446eb38b46022
MontyPan <monty.pan@dontcareabout.us>
2018-06-14 21:41:22 +0800

    日期格式

---
+++ src/main/java/us/dontcareabout/fx/client/Util.java
@@ -0,0 +1,7 @@
+package us.dontcareabout.fx.client;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+public class Util {
+	public static final DateTimeFormat DATE_FORMAT = DateTimeFormat.getFormat("yyyy/MM/dd");
+}
