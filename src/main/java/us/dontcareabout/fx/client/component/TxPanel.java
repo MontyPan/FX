a3a171a9d18b9991e3643ad9f658bea3e7d4a606
MontyPan <monty.pan@dontcareabout.us>
2018-06-20 16:41:01 +0800

    修改 setType() 格式以及增加 transcation() 實作

--- src/main/java/us/dontcareabout/fx/client/component/TxPanel.java
+++ src/main/java/us/dontcareabout/fx/client/component/TxPanel.java
@@ -5,8 +5,17 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.chart.client.draw.RGB;
+import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent;
+import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent.SpriteSelectionHandler;
+import com.sencha.gxt.core.client.util.DateWrapper;
 import com.sencha.gxt.widget.core.client.Composite;
+import com.sencha.gxt.widget.core.client.form.DateField;
+import com.sencha.gxt.widget.core.client.form.DoubleField;
+import com.sencha.gxt.widget.core.client.form.TextField;
 
+import us.dontcareabout.fx.client.data.DataCenter;
+import us.dontcareabout.fx.shared.Currency;
+import us.dontcareabout.fx.shared.ForeignTX;
 import us.dontcareabout.gxt.client.draw.component.TextButton;
 import us.dontcareabout.gxt.client.draw.container.SimpleLayerContainer;
 
@@ -16,9 +25,17 @@ public class TxPanel extends Composite {
 
 	@UiField(provided=true) SimpleLayerContainer titleLayer;
 	@UiField(provided=true) SimpleLayerContainer submitLayer;
+	@UiField DateField date;
+	@UiField DoubleField rate;
+	@UiField DoubleField value;
+	@UiField TextField note;
 
 	private TextButton titleBtn = new TextButton();
 	private TextButton submitBtn = new TextButton("確定送出");
+
+	/** true 是買入、false 是賣出 */
+	private boolean txType;
+	private Currency currency;
 
 	public TxPanel() {
 		titleLayer = new SimpleLayerContainer(titleBtn);
@@ -27,11 +44,30 @@ public class TxPanel extends Composite {
 
 		submitBtn.setBgRadius(10);
 		submitBtn.setTextColor(RGB.WHITE);
+		submitBtn.addSpriteSelectionHandler(new SpriteSelectionHandler() {
+			@Override
+			public void onSpriteSelect(SpriteSelectionEvent event) {
+				transaction();
+			}
+		});
 	}
 
-	public void setType(boolean isBuy) {
+	public void setType(Currency c, boolean isBuy) {
+		currency = c;
+		txType = isBuy;
 		titleBtn.setText(isBuy ? "買進來" : "賣出去");
 		titleBtn.setTextColor(isBuy ? RGB.RED : RGB.BLUE);
 		submitBtn.setBgColor(isBuy ? RGB.RED : RGB.BLUE);
+	}
+
+	private void transaction() {
+		//懶得用 editor framework 了...... Zzzz
+		ForeignTX foreignTX = new ForeignTX();
+		foreignTX.setCurrency(currency);
+		foreignTX.setDate(date.getValue() == null ? new DateWrapper().clearTime().asDate() : date.getValue());
+		foreignTX.setNote(note.getValue());
+		foreignTX.setRate(rate.getValue());
+		foreignTX.setValue(txType ? value.getValue() : value.getValue() * -1);
+		DataCenter.transaction(foreignTX);
 	}
 }
