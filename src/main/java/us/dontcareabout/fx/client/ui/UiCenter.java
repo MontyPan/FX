dae680994dfa31f9be03005955dd08113aa63c2c
MontyPan <monty.pan@dontcareabout.us>
2018-06-20 16:53:27 +0800

    增加 dialog 控制功能

--- src/main/java/us/dontcareabout/fx/client/ui/UiCenter.java
+++ src/main/java/us/dontcareabout/fx/client/ui/UiCenter.java
@@ -1,10 +1,21 @@
 package us.dontcareabout.fx.client.ui;
 
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.Window;
 import com.sencha.gxt.widget.core.client.container.Viewport;
 
 public class UiCenter {
 	private final static Viewport viewport = new Viewport();
+	static {
+		RootPanel.get().add(viewport);
+	}
+
+	private final static Window dialog = new Window();
+	static {
+		dialog.setModal(true);
+		dialog.setResizable(false);
+	}
 
 	public static void mask(String message) {
 		viewport.mask(message);
@@ -16,6 +27,18 @@ public class UiCenter {
 
 	public static void unmask() {
 		viewport.unmask();
+	}
+
+	public static void closeDialog() {
+		dialog.hide();
+	}
+
+	private static void dialog(Widget widget, int width, int height) {
+		dialog.clear();
+		dialog.add(widget);
+		dialog.show();
+		dialog.setPixelSize(width, height);
+		dialog.center();
 	}
 
 	private static void switchTo(Widget widget) {
