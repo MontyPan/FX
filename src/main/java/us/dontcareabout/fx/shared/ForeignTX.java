6c78001a0c4358d9f6d05ffa5b59866448732aa3
MontyPan <monty.pan@dontcareabout.us>
2018-06-25 23:44:55 +0800

    增加：紀錄買入紀錄的餘額與賣出紀錄的獲利數據

--- src/main/java/us/dontcareabout/fx/shared/ForeignTX.java
+++ src/main/java/us/dontcareabout/fx/shared/ForeignTX.java
@@ -3,6 +3,8 @@ package us.dontcareabout.fx.shared;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.google.common.base.Preconditions;
+
 public class ForeignTX implements Serializable, HasId{
 	private static final long serialVersionUID = 1L;
 
@@ -22,6 +24,12 @@ public class ForeignTX implements Serializable, HasId{
 	private double rate;
 
 	private String note;
+
+	/**
+	 * 對於買入的紀錄而言，意義為尚未賣出的餘額（參見 {@link #getBalance()}），
+	 * 對於賣出的紀錄而言，意義為該筆賣出的獲利（參見 {@link #getProfit()}）。
+	 */
+	private double balance;
 
 	@Override
 	public int getId() {
@@ -77,6 +85,7 @@ public class ForeignTX implements Serializable, HasId{
 
 	public void setValue(double value) {
 		this.value = value;
+		this.balance = value > 0 ? value : 0;
 	}
 
 	public double getRate() {
@@ -93,5 +102,37 @@ public class ForeignTX implements Serializable, HasId{
 
 	public void setNote(String note) {
 		this.note = note;
+	}
+
+	public void setProfit(double v) {
+		Preconditions.checkState(value < 0, "買入的紀錄不計算利潤");
+		balance = v;
+	}
+
+	/**
+	 * 只有賣出（{@link #value} < 0）的紀錄才有意義。
+	 *
+	 * @return 賣出時的利潤
+	 */
+	public double getProfit() {
+		return value < 0 ? balance : 0;
+	}
+
+	/**
+	 * 只有買入（{@link #value} > 0）的紀錄才有意義。
+	 * 在賣出時會扣掉對應的數值，直到為 0。
+	 *
+	 * @return 該筆交易還未賣出的餘額
+	 */
+	public double getBalance() {
+		return value > 0 ? balance : 0;
+	}
+
+	public void sell(double v) {
+		Preconditions.checkArgument(v > 0, "賣出負數？不用這麼扭曲吧？");
+		Preconditions.checkState(value > 0, "賣出的紀錄不能再賣出");
+		Preconditions.checkState(balance >= value, "餘額不足");
+
+		balance -= v;
 	}
 }
