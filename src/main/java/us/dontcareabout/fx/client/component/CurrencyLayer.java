f076a4ab26b54fc818957adba207b428b7b8e51d
MontyPan <monty.pan@dontcareabout.us>
2018-06-28 15:44:52 +0800

    移除「買入」、「賣出」按鈕。調整資訊顯示順序。

--- src/main/java/us/dontcareabout/fx/client/component/CurrencyLayer.java
+++ src/main/java/us/dontcareabout/fx/client/component/CurrencyLayer.java
@@ -1,15 +1,11 @@
 package us.dontcareabout.fx.client.component;
 
 import com.sencha.gxt.chart.client.draw.RGB;
-import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent;
-import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent.SpriteSelectionHandler;
 
-import us.dontcareabout.fx.client.Util;
 import us.dontcareabout.fx.client.data.DataCenter;
 import us.dontcareabout.fx.client.data.ForeignTxReadyEvent;
 import us.dontcareabout.fx.client.data.ForeignTxReadyEvent.ForeignTxReadyHandler;
 import us.dontcareabout.fx.client.data.TxSummary;
-import us.dontcareabout.fx.client.ui.UiCenter;
 import us.dontcareabout.fx.shared.Currency;
 import us.dontcareabout.fx.shared.tool.CurrencyUtil;
 import us.dontcareabout.gxt.client.draw.LTextSprite;
@@ -25,8 +21,6 @@ public class CurrencyLayer extends LayerSprite {
 	private LTextSprite costTS = new LTextSprite();
 	private LTextSprite cashOutTS = new LTextSprite();
 	private LTextSprite rateTS = new LTextSprite();
-	private TextButton buyBtn = new TextButton("買入");
-	private TextButton sellBtn = new TextButton("賣出");
 
 	public CurrencyLayer(Currency c) {
 		currency = c;
@@ -37,8 +31,6 @@ public class CurrencyLayer extends LayerSprite {
 		add(costTS);
 		add(cashOutTS);
 		add(rateTS);
-		add(buyBtn);
-		add(sellBtn);
 
 		nameTS.setTextColor(RGB.WHITE);
 		nameTS.setBgColor(RGB.BLUE);
@@ -48,23 +40,6 @@ public class CurrencyLayer extends LayerSprite {
 		makeUp(costTS);
 		makeUp(rateTS);
 		makeUp(cashOutTS);
-
-		makeUp(buyBtn);
-		makeUp(sellBtn);
-
-		buyBtn.addSpriteSelectionHandler(new SpriteSelectionHandler() {
-			@Override
-			public void onSpriteSelect(SpriteSelectionEvent event) {
-				UiCenter.txDialog(currency, true);
-			}
-		});
-
-		sellBtn.addSpriteSelectionHandler(new SpriteSelectionHandler() {
-			@Override
-			public void onSpriteSelect(SpriteSelectionEvent event) {
-				UiCenter.txDialog(currency, false);
-			}
-		});
 
 		DataCenter.addForeignTxReady(new ForeignTxReadyHandler() {
 			@Override
@@ -94,35 +69,22 @@ public class CurrencyLayer extends LayerSprite {
 		nameTS.setLY(0);
 		nameTS.resize(getWidth(), 30);
 
-		balanceTS.setLX(20);
-		balanceTS.setLY(60);
+		rateTS.setLX(10);
+		rateTS.setLY(35);
 
-		costTS.setLX(20);
-		costTS.setLY(90);
+		balanceTS.setLX(10);
+		balanceTS.setLY(65);
 
-		rateTS.setLX(20);
-		rateTS.setLY(130);
+		costTS.setLX(10);
+		costTS.setLY(85);
 
-		cashOutTS.setLX(20);
-		cashOutTS.setLY(160);
-
-		buyBtn.setLX(5);
-		buyBtn.setLY(getHeight() - 35 - Util.SCROLL_HEIGHT);
-		buyBtn.resize(80, 30);
-
-		sellBtn.setLX(90);
-		sellBtn.setLY(getHeight() - 35 - Util.SCROLL_HEIGHT);
-		sellBtn.resize(80, 30);
-	}
-
-	private void makeUp(TextButton btn) {
-		btn.setBgColor(RGB.YELLOW);
-		btn.setBgRadius(10);
+		cashOutTS.setLX(10);
+		cashOutTS.setLY(105);
 	}
 
 	private void makeUp(LTextSprite ts) {
 		ts.setFill(RGB.WHITE);
-		ts.setFontSize(22);
+		ts.setFontSize(18);
 	}
 
 	private void update() {
