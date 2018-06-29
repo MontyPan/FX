1b9bb43cb9192964c0c5149bd45d2188d468f745
MontyPan <monty.pan@dontcareabout.us>
2018-06-29 21:24:33 +0800

    增加 number format

--- src/main/java/us/dontcareabout/fx/client/Util.java
+++ src/main/java/us/dontcareabout/fx/client/Util.java
@@ -1,8 +1,10 @@
 package us.dontcareabout.fx.client;
 
 import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.NumberFormat;
 
 public class Util {
 	public static final int SCROLL_HEIGHT = 16;
 	public static final DateTimeFormat DATE_FORMAT = DateTimeFormat.getFormat("yyyy/MM/dd");
+	public static final NumberFormat NUMBER_FORMAT = NumberFormat.getFormat("#####.##");
 }
