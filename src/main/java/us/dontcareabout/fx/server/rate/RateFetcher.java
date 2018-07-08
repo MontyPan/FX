0f7ea543943d8caa2fe17dc6e652efb6018b67dd
MontyPan <monty.pan@dontcareabout.us>
2018-07-08 20:17:29 +0800

    增加：一定時間內會直接回傳 cache 值而不實際抓取銀行資料

--- src/main/java/us/dontcareabout/fx/server/rate/RateFetcher.java
+++ src/main/java/us/dontcareabout/fx/server/rate/RateFetcher.java
@@ -1,5 +1,6 @@
 package us.dontcareabout.fx.server.rate;
 
+import java.util.Date;
 import java.util.HashMap;
 
 import us.dontcareabout.fx.shared.Currency;
@@ -7,18 +8,27 @@ import us.dontcareabout.fx.shared.Currency;
 public abstract class RateFetcher {
 	HashMap<Currency, Double> map = new HashMap<>();
 
+	private Date lastUpdate = new Date(0);
+
 	static {
 		//HTTPS 連線需要，統一放在這裡
 		System.setProperty("https.protocols", "TLSv1.2");
 	}
 
 	public final HashMap<Currency, Double> get(){
-		fetchAndParse();
+		Date now = new Date();
+
+		//TODO 時間間隔抽成設定檔
+		if (now.getTime() - lastUpdate.getTime() > 60 * 1000) {
+			fetchAndParse();
+			lastUpdate = now;
+		}
+
 		return map;
 	}
 
 	/**
-	 * 實作抓網頁、取值並塞進 {@link #map} 的各銀行實作
+	 * 抓網頁、取值並塞進 {@link #map} 的各銀行實作
 	 */
 	abstract void fetchAndParse();
 }
