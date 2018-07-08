554f5faa6b70b99398bc0021f3bc26c581413653
MontyPan <monty.pan@dontcareabout.us>
2018-07-08 23:08:42 +0800

    用 ListStore 作 model，並依持有成本排序

--- src/main/java/us/dontcareabout/fx/client/component/CurrencyMarquee.java
+++ src/main/java/us/dontcareabout/fx/client/component/CurrencyMarquee.java
@@ -1,9 +1,28 @@
 package us.dontcareabout.fx.client.component;
 
+import java.util.Comparator;
 import java.util.HashMap;
 
+import com.google.common.collect.ComparisonChain;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.sencha.gxt.core.client.dom.ScrollSupport.ScrollMode;
 import com.sencha.gxt.core.client.util.Margins;
+import com.sencha.gxt.data.shared.ListStore;
+import com.sencha.gxt.data.shared.ModelKeyProvider;
+import com.sencha.gxt.data.shared.SortDir;
+import com.sencha.gxt.data.shared.Store;
+import com.sencha.gxt.data.shared.Store.StoreFilter;
+import com.sencha.gxt.data.shared.Store.StoreSortInfo;
+import com.sencha.gxt.data.shared.event.StoreAddEvent;
+import com.sencha.gxt.data.shared.event.StoreClearEvent;
+import com.sencha.gxt.data.shared.event.StoreDataChangeEvent;
+import com.sencha.gxt.data.shared.event.StoreFilterEvent;
+import com.sencha.gxt.data.shared.event.StoreHandlers;
+import com.sencha.gxt.data.shared.event.StoreRecordChangeEvent;
+import com.sencha.gxt.data.shared.event.StoreRemoveEvent;
+import com.sencha.gxt.data.shared.event.StoreSortEvent;
+import com.sencha.gxt.data.shared.event.StoreUpdateEvent;
 import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer;
 
 import us.dontcareabout.fx.client.data.DataCenter;
@@ -17,6 +36,12 @@ import us.dontcareabout.fx.shared.Currency;
 public 	class CurrencyMarquee extends HorizontalLayoutContainer {
 	private static final HorizontalLayoutData CURRENCY_HLD = new HorizontalLayoutData(140, 1, new Margins(1));
 
+	private final ListStore<TxSummary> store = new ListStore<>(new ModelKeyProvider<TxSummary>() {
+		@Override
+		public String getKey(TxSummary item) {
+			return item.currency.name();
+		}
+	});
 	private final HashMap<Currency, CurrencyLayer> currencyMap = new HashMap<>();
 
 	public CurrencyMarquee() {
@@ -41,24 +66,76 @@ public 	class CurrencyMarquee extends HorizontalLayoutContainer {
 				update();
 			}
 		});
+		store.addStoreHandlers(new StoreHandlers<TxSummary>(){
+			@Override
+			public void onAdd(StoreAddEvent<TxSummary> event) { refresh(); }
+
+			@Override
+			public void onRemove(StoreRemoveEvent<TxSummary> event) { refresh(); }
+
+			@Override
+			public void onFilter(StoreFilterEvent<TxSummary> event) { refresh(); }
+
+			@Override
+			public void onClear(StoreClearEvent<TxSummary> event) { refresh(); }
+
+			@Override
+			public void onUpdate(StoreUpdateEvent<TxSummary> event) { refresh(); }
+
+			@Override
+			public void onDataChange(StoreDataChangeEvent<TxSummary> event) { refresh(); }
+
+			@Override
+			public void onRecordChange(StoreRecordChangeEvent<TxSummary> event) { refresh(); }
+
+			@Override
+			public void onSort(StoreSortEvent<TxSummary> event) { refresh(); }
+		});
+		store.addFilter(new StoreFilter<TxSummary>() {
+			@Override
+			public boolean select(Store<TxSummary> store, TxSummary parent, TxSummary item) {
+				//只留下還有餘額的貨幣
+				return item.balance != 0;
+			}
+		});
+		store.setEnableFilters(true);
+		store.addSortInfo(
+			new StoreSortInfo<TxSummary>(
+				new Comparator<TxSummary>() {
+					@Override
+					public int compare(TxSummary o1, TxSummary o2) {
+						return ComparisonChain.start().compare(o1.cost, o2.cost).result();
+					}
+				},
+				SortDir.DESC
+			)
+		);
 	}
 
 	private void update() {
 		if (!DataCenter.isAllReady()) { return; }
 
-		clear();
+		store.clear();
 
-		//只留下還有餘額的貨幣
 		for (Currency c : Currency.values()) {
 			if (c == Currency.NTD) { continue; }
 
-			TxSummary sum = DataCenter.getSummary(c);
+			store.add(DataCenter.getSummary(c));
+		}
+	}
 
-			if (sum.balance == 0) { continue; }
+	private void refresh() {
+		clear();
 
-			add(currencyMap.get(c), CURRENCY_HLD);
+		for (TxSummary sum : store.getAll()) {
+			add(currencyMap.get(sum.currency), CURRENCY_HLD);
 		}
 
-		forceLayout();
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			@Override
+			public void execute() {
+				forceLayout();
+			}
+		});
 	}
 }
