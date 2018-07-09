83afdf6396284984f40d093916254d9e0836e955
MontyPan <monty.pan@dontcareabout.us>
2018-07-09 15:56:01 +0800

    正式版本 entry point

--- src/main/java/us/dontcareabout/fx/client/FXEP.java
+++ src/main/java/us/dontcareabout/fx/client/FXEP.java
@@ -1,10 +1,64 @@
 package us.dontcareabout.fx.client;
 
-import com.google.gwt.core.client.EntryPoint;
+import com.sencha.gxt.core.shared.event.GroupingHandlerRegistration;
 
-public class FXEP implements EntryPoint {
+import us.dontcareabout.fx.client.data.CapitalTxReadyEvent;
+import us.dontcareabout.fx.client.data.CapitalTxReadyEvent.CapitalTxReadyHandler;
+import us.dontcareabout.fx.client.data.DataCenter;
+import us.dontcareabout.fx.client.data.ForeignTxReadyEvent;
+import us.dontcareabout.fx.client.data.ForeignTxReadyEvent.ForeignTxReadyHandler;
+import us.dontcareabout.fx.client.data.RateReadyEvent;
+import us.dontcareabout.fx.client.data.RateReadyEvent.RateReadyHandler;
+import us.dontcareabout.fx.client.ui.UiCenter;
+import us.dontcareabout.gwt.client.GFEP;
+
+public class FXEP extends GFEP {
 	@Override
-	public void onModuleLoad() {
+	protected String version() { return "0.0.1"; }
 
+	@Override
+	protected String defaultLocale() { return "zh_TW"; }
+
+	private int counter = 3;
+	private GroupingHandlerRegistration ghr = new GroupingHandlerRegistration();
+	private void initCheck() {
+		counter--;
+		if (counter == 0) {
+			ghr.removeHandler();
+			UiCenter.unmask();
+		}
+	}
+
+	@Override
+	protected void start() {
+		UiCenter.mainView();
+		ghr.add(
+			DataCenter.addCapitalTxReady(new CapitalTxReadyHandler() {
+				@Override
+				public void onCapitalTxReady(CapitalTxReadyEvent event) {
+					initCheck();
+				}
+			})
+		);
+		ghr.add(
+			DataCenter.addForeignTxReady(new ForeignTxReadyHandler() {
+				@Override
+				public void onForeignTxReady(ForeignTxReadyEvent event) {
+					initCheck();
+				}
+			})
+		);
+		ghr.add(
+			DataCenter.addRateReady(new RateReadyHandler() {
+				@Override
+				public void onRateReady(RateReadyEvent event) {
+					initCheck();
+				}
+			})
+		);
+		UiCenter.processing();
+		DataCenter.wantCapitalList();
+		DataCenter.wantForeignList();
+		DataCenter.wantRate();
 	}
 }
