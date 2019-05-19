8b9fee9d75fc07f8c5e621cdc997986166eee5c9
MontyPan <monty.pan@dontcareabout.us>
2019-05-19 16:37:56 +0800

    增加：AlertLayer

--- src/main/java/us/dontcareabout/fx/client/component/CurrencyTitleLayer.java
+++ src/main/java/us/dontcareabout/fx/client/component/CurrencyTitleLayer.java
@@ -9,13 +9,16 @@ import us.dontcareabout.fx.client.ui.ChangeCurrencyEvent.ChangeCurrencyHandler;
 import us.dontcareabout.fx.client.ui.UiCenter;
 import us.dontcareabout.fx.shared.Currency;
 import us.dontcareabout.fx.shared.tool.CurrencyUtil;
+import us.dontcareabout.gxt.client.draw.LTextSprite;
 import us.dontcareabout.gxt.client.draw.LayerSprite;
 import us.dontcareabout.gxt.client.draw.component.TextButton;
+import us.dontcareabout.gxt.client.draw.util.TextUtil;
 
 public class CurrencyTitleLayer extends LayerSprite {
 	private TextButton currencyTB = new TextButton("請選擇貨幣");
 	private TextButton buyTB = new TextButton("買入");
 	private TextButton sellTB = new TextButton("賣出");
+	private AlertLayer alertLayer = new AlertLayer();
 
 	private Currency currency;
 
@@ -55,6 +58,9 @@ public class CurrencyTitleLayer extends LayerSprite {
 		});
 		add(sellTB);
 
+		alertLayer.setBgRadius(5);
+		add(alertLayer);
+
 		UiCenter.addChangeCurrency(new ChangeCurrencyHandler() {
 			@Override
 			public void onChangeCurrency(ChangeCurrencyEvent event) {
@@ -80,5 +86,47 @@ public class CurrencyTitleLayer extends LayerSprite {
 		sellTB.setLX(290);
 		sellTB.setLY(10);
 		sellTB.resize(80, 40);
+
+		alertLayer.setLX(getWidth() - 410);
+		alertLayer.setLY(5);
+		alertLayer.resize(390, 50);
+	}
+
+	private class AlertLayer extends LayerSprite {
+		private LTextSprite buyTitle = new LTextSprite("買匯：");
+		private LTextSprite buyMin = new LTextSprite("3.1415");
+		private LTextSprite sellTitle = new LTextSprite("賣匯：");
+		private LTextSprite sellMax = new LTextSprite("19.85");
+
+		AlertLayer() {
+			setBgColor(RGB.ORANGE);
+
+			buyTitle.setFontSize(24);
+			buyTitle.setLX(10);
+			buyMin.setLX(85);
+			sellTitle.setFontSize(24);
+			sellTitle.setLX(200);
+			sellMax.setLX(275);
+
+			add(buyTitle);
+			add(buyMin);
+			add(sellTitle);
+			add(sellMax);
+		}
+
+		@Override
+		protected void adjustMember() {
+			TextUtil.autoResize(buyMin, 100, 35);
+			TextUtil.autoResize(sellMax, 100, 35);
+
+			centerY(buyTitle);
+			centerY(buyMin);
+			centerY(sellTitle);
+			centerY(sellMax);
+		}
+
+		private void centerY(LTextSprite ts) {
+			ts.setLY((getHeight() - ts.getBBox().getHeight()) / 2 + TextUtil.getYOffset(ts));
+		}
 	}
 }
